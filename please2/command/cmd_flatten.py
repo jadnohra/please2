5dce5138b685d87551b37d9497363e44b5901a52
Jad Nohra <jadnohra@gmail.com>
2019-04-25 23:21:01 +0200

    WIP: flatten command, need to pickup highlights

--- please2/command/cmd_flatten.py
+++ please2/command/cmd_flatten.py
@@ -10,10 +10,27 @@ class CommandFlatten(Command):
         return 'flatten'
 
     def run_match(self, params):
-        print(params['prev_result'])
-        if isinstance(params['prev_result'], TreeNode):
-            print('GOT IT')
-        result = {
+        def find_tree(params):
+            for k,v in params.get('prev_result', {}).items():
+                if isinstance(v, TreeNode):
+                    return k, v
+            return (None, None)
+        def join(a, b):
+            return a + '/' + b
+        def flatten_tree(node, path, flat):
+            node_path = join(path, node.name())
+            if node.is_leaf():
+                flat.append(node_path)
+            else:
+                for child in node.children():
+                    flatten_tree(child, node_path, flat)
+        tree_k, tree_v = find_tree(params)
+        result = {}
+        if tree_k is not None:
+            flat_tree = []
+            flatten_tree(tree_v, '', flat_tree)
+            result = {
+                tree_k+'-flat': flat_tree
             }
         return Match(result)
 
