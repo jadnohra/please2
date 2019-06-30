0db2dce40dd0424781936db2fcf0615dae51ba0f
Jad Nohra <jadnohra@gmail.com>
2019-06-30 13:15:14 +0200

    Added forgotten file update

--- please2/all_commands.py
+++ please2/all_commands.py
@@ -6,7 +6,7 @@ import please2.command.cmd_filter
 import please2.command.cmd_filter_out
 import please2.command.cmd_printers_ls
 import please2.command.cmd_print
-import please2.command.cmd_set_audio_volume
+import please2.command.cmd_set_audio_volumepl
 import please2.command.git.cmd_git_help
 import please2.command.git.cmd_git_ls_remotes
 import please2.command.git.cmd_git_which_branch
